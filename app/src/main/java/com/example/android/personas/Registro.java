22a85b4286a78f073079f6748c39db2d75b54f43
Leiner <leiner_0304@hotmail.com>
2017-04-18 20:46:27 -0500

    Listar

--- app/src/main/java/com/example/android/personas/Registro.java
+++ app/src/main/java/com/example/android/personas/Registro.java
@@ -1,13 +1,61 @@
 package com.example.android.personas;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class Registro extends AppCompatActivity {
-
+    private EditText nomb, apell, edad;
+    private CheckBox leer, bailar, programar;
+    private Resources res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        res = this.getResources();
+        nomb=(EditText)findViewById(R.id.txtNombre);
+        apell= (EditText)findViewById(R.id.txtApellido);
+        edad = (EditText)findViewById(R.id.txtEdad);
+
+        leer=(CheckBox)findViewById(R.id.chkLeer);
+        bailar=(CheckBox)findViewById(R.id.chkBailar);
+        programar=(CheckBox)findViewById(R.id.chkProgramar);
+
+
+
+
     }
+
+    public void registrar(View v){
+        String nombre, apellido, aux="";
+        int ed;
+        nombre=nomb.getText().toString().trim();
+        apellido=apell.getText().toString().trim();
+        ed= Integer.parseInt(edad.getText().toString().trim());
+        if(leer.isChecked()) aux=res.getString(R.string.leer);
+        if(leer.isChecked() && bailar.isChecked()) aux=aux+", ";
+        if(leer.isChecked() && programar.isChecked()) aux=aux+", ";
+        if(bailar.isChecked()) aux= aux+res.getString((R.string.bailar));
+        if(bailar.isChecked() && programar.isChecked()) aux=aux+", ";
+        if(programar.isChecked()) aux= aux+res.getString((R.string.programar));
+
+
+        Persona p = new Persona(nombre,apellido,ed,aux);
+        p.guardar();
+        limpiar();
+    }
+
+    public void limpiar(){
+        nomb.setText("");
+        apell.setText("");
+        edad.setText("");
+        leer.setChecked(true);
+        bailar.setChecked(false);
+        programar.setChecked(false);
+    }
+
 }
